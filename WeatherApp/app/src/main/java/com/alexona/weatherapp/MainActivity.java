7ce28bdb023e1ac9f7ab8b24006f525ba84a3da4
muirurikin <muirurikin@gmail.com>
2016-10-13 13:12:31 +0300

    Add Packages

--- WeatherApp/app/src/main/java/com/alexona/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/alexona/weatherapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.alexona.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the Menu -> Adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
