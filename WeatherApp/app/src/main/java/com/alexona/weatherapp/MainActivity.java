667d4093f3c9550d92dc5621fc60010c6c61fa68
muirurikin <muirurikin@gmail.com>
2016-10-14 09:30:51 +0300

    Update Main activity

--- WeatherApp/app/src/main/java/com/alexona/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/alexona/weatherapp/MainActivity.java
@@ -1,17 +1,85 @@
 package com.alexona.weatherapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import data.JSONWeatherParser;
+import data.WeatherHttpClient;
+import model.Weather;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView cityName;
+    private TextView temp;
+    private ImageView iconView;
+    private TextView description;
+    private TextView humididty;
+    private TextView pressure;
+    private TextView wind;
+    private TextView sunrise;
+    private TextView sunset;
+    private TextView updated;
+
+    Weather weather = new Weather();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cityName = (TextView) findViewById(R.id.cityText);
+        temp = (TextView) findViewById(R.id.tempText);
+        iconView = (ImageView) findViewById(R.id.thumbnailIcon);
+        description = (TextView) findViewById(R.id.cloudText);
+        humididty = (TextView) findViewById(R.id.humidText);
+        pressure = (TextView) findViewById(R.id.pressureText);
+        wind = (TextView) findViewById(R.id.windText);
+        sunrise = (TextView) findViewById(R.id.riseText);
+        sunset = (TextView) findViewById(R.id.setText);
+        updated = (TextView) findViewById(R.id.updateText);
+
+        renderWeatherData("Spokane,US");
+
     }
+
+
+    public void renderWeatherData(String city) {
+
+        WeatherTask weatherTask = new WeatherTask();
+        weatherTask.execute(new String[]{city + "&units=metric"});
+
+    }
+
+    private class WeatherTask extends AsyncTask<String, Void, Weather> {
+        @Override
+        protected Weather doInBackground(String... params) {
+
+            String data = ( (new WeatherHttpClient()).getWeatherData(params[0]));
+
+            weather = JSONWeatherParser.getWeather(data);
+
+            Log.v("Data: ", weather.place.getCity());
+
+
+            return weather;
+
+        }
+
+        @Override
+        protected void onPostExecute(Weather weather) {
+            super.onPostExecute(weather);
+        }
+    }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +87,18 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Handle action bar item clicks here.
 
+
+        int id = item.getItemId();
+
+        // No  inspection simplifiableIf Statement
+        if (id == R.id.change_cityId) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
