b76f6705ab88ddc55188ae300bb7aa9fa834e0d4
muirurikin <muirurikin@gmail.com>
2016-10-14 22:31:21 +0300

    Added CityPreference.java

--- WeatherApp/app/src/main/java/com/alexona/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/alexona/weatherapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.alexona.weatherapp;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.icu.text.DateFormat;
 import android.icu.text.DecimalFormat;
+import android.net.http.HttpResponseCache;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,8 +14,11 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
 
+import Util.Utils;
 import data.JSONWeatherParser;
 import data.WeatherHttpClient;
 import model.Weather;
@@ -64,12 +69,44 @@ public class MainActivity extends AppCompatActivity {
 
     private class DownloadImageAsyncTask extends AsyncTask<String, Void, Bitmap> {
         @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            super.onPostExecute(bitmap);
+        protected Bitmap doInBackground(String... params) {
+            return downloadImage(params[0]);
         }
 
         @Override
-        protected Bitmap doInBackground(String... params) {
+        protected void onPostExecute(Bitmap bitmap) {
+            iconView.setImageBitmap(bitmap);
+//            super.onPostExecute(bitmap);
+        }
+
+        private Bitmap downloadImage(String code) {
+            final DefaultHttpClient defaultHttpClient = new DefaultHttpClient();
+
+//            final HttpGet getRequest = new HttpGet(Utils.ICON_URL + code + ".png");
+            final HttpGet getRequest = new HttpGet("http://www.9ori.com/store/media/images/8ab579a656.jpg");
+
+            try {
+            HttpResponse response = client.execute(getRequest);
+
+                final int statusCode = response.getStatusLine().getStatusCode();
+
+                if ( statusCode != HttpStatus.SC_OK) {
+                    Log.e("DownloadImage", "Error:" + statusCode);
+                }
+                final HttpEntity entity = response.getEntity();
+
+                if (entity != null) {
+                    InputStream inputStream = null;
+
+                    inputStream = entity.getContent();
+
+                    //Decode contents from the stream
+                    final Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                    return bitmap;
+                }
+        } catch (IOException) {
+                e.printStackTrace();
+            }
             return null;
         }
     }
@@ -80,9 +117,13 @@ public class MainActivity extends AppCompatActivity {
 
             String data = ( (new WeatherHttpClient()).getWeatherData(params[0]));
 
+            weather.iconData = weather.currentCondition.getIcon();
+
             weather = JSONWeatherParser.getWeather(data);
 
             Log.v("Data: ", weather.currentCondition.getDescription());
+
+            new DownloadImageAsyncTask().execute(weather.iconData);
 
 
             return weather;
---
+++ WeatherApp/app/src/main/java/data/CityPreference.java
@@ -0,0 +1,22 @@
+package data;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+/**
+ * Created by alexona on 10/14/16.
+ */
+
+public class CityPreference {
+    SharedPreferences prefs;
+
+    public CityPreference(Activity activity) {
+        prefs = activity.getPreferences(Activity.MODE_PRIVATE);
+    }
+    public String getCity() {
+        return prefs.getString("city", "Nairobi,KE");
+    }
+    public void setCity(String city) {
+        prefs.edit().putString("city",  city).commit();
+    }
+}
